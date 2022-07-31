78940da624f3e526a3b4db5412d579f122e5b655
Yannick Schurter <yannick.schurter@gmail.com>
2022-07-31 21:10:56 +0200

    feat: basic jump implementation

--- index.js
+++ index.js
@@ -14,19 +14,19 @@ attackCanvas.height = 576;
 const playerInformation = {
   position: {
     x: 200,
-    y: 300,
+    y: 0,
   },
   color: "blue",
   velocity: 10,
   orientation: Direction.right,
-  directionKey: {left: "Q", down: "S", right: "D", up: "Z"}, 
+  directionKey: {left: "Q", down: "S", right: "D", up: "Z"},
   attackKey: {attack: " ",},
 };
 
 const enemyInformation = {
   position: {
     x: 701,
-    y: 300,
+    y: 0,
   },
   color: "red",
   velocity: 10,
@@ -36,28 +36,24 @@ const enemyInformation = {
 };
 
 const player = new Personnage(playerInformation);
-draw(cPerson, player.color, player.position.x, player.position.y, player.width, player.height);
 player.getWeapon({xRange :100,damage: 10,attackDuration: 500,cooldown: 10});
 
 const enemy = new Personnage(enemyInformation);
-draw(cPerson, enemy.color, enemy.position.x, enemy.position.y, enemy.width, enemy.height);
 enemy.getWeapon({xRange: 200,damage: 10,attackDuration: 500,cooldown: 10});
 
 window.addEventListener("keydown", (event) => {
   if (!event.repeat) {
-    console.log(event.key);
     const pressKey = event.key.toUpperCase();
-  
+
     if (Object.values(player.directionKey).includes(pressKey) || Object.values(player.attackKey).includes(pressKey)) {
       player.action(pressKey);
     } else if (Object.values(enemy.directionKey).includes(pressKey) || Object.values(enemy.attackKey).includes(pressKey)) {
       enemy.action(pressKey);
-    } 
+    }
   }
 });
 
 window.addEventListener("keyup", (event) => {
-  console.log(event.key);
   const pressKey = event.key.toUpperCase();
 
   queueAction.forEach((item) => {
